e4d44c43867c4b7dd80b330528a225eb857d474f
Oggal <Philip.dan.taylor@gmail.com>
2023-04-18 12:02:06 -0700

    Post Live Code additon

--- main.h
+++ main.h
@@ -25,7 +25,6 @@ typedef struct binary_tree_s binary_tree_t;
 void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *add_node(binary_tree_t *parent, int value);
-
 binary_tree_t *create_node(int value);
-
+int count_nodes(binary_tree_t *tree);
 #endif
